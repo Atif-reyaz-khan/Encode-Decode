de0f3e36bf53b48a8f14d3b6cdc1a94f62349281
Mohd Atif Reyaz Khan <48691336+Atif-reyaz-khan@users.noreply.github.com>
2021-03-08 10:09:49 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var messageInput = document.getElementById('message');
 
 var textCanvas = document.getElementById('textCanvas');
 var tctx = textCanvas.getContext('2d');
-
+var dwn = document.getElementById('btndownload');
 
 function handleImage(e){
     var reader = new FileReader();
@@ -53,8 +53,33 @@ function handleImage(e){
 }
 
 
-
-
+dwn.onclick = function(){
+    download(canvas, 'myimage.png');
+  }
+  function download(canvas, filename) {
+    /// create an "off-screen" anchor tag
+    var lnk = document.createElement('a'), e;
+  
+    /// the key here is to set the download attribute of the a tag
+    lnk.download = filename;
+  
+    /// convert canvas content to data-uri for link. When download
+    /// attribute is set the content pointed to by link will be
+    /// pushed as "download" in HTML5 capable browsers
+    lnk.href = canvas.toDataURL("image/png;base64");
+  
+    /// create a "fake" click-event to trigger the download
+    if (document.createEvent) {
+      e = document.createEvent("MouseEvents");
+      e.initMouseEvent("click", true, true, window,
+                       0, 0, 0, 0, 0, false, false, false,
+                       false, 0, null);
+  
+      lnk.dispatchEvent(e);
+    } else if (lnk.fireEvent) {
+      lnk.fireEvent("onclick");
+    }
+  }
 
 var decodeCanvas = document.getElementById('imageCanvas2');
 var dctx = decodeCanvas.getContext('2d');
